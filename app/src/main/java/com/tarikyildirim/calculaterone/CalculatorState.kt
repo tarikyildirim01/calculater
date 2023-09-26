8409ff568c7b85b80085e61720b9e3e33008ed63
Tarık Yıldırım <bak-kal@hotmail.com>
2023-09-26 17:31:22 +0300

    first commit - it is ready version

---
+++ app/src/main/java/com/tarikyildirim/calculaterone/CalculatorState.kt
@@ -0,0 +1,7 @@
+package com.tarikyildirim.calculaterone
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
